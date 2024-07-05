ba7804b0736197c4b7b3c1f80bf08b321199bf22
[Mariam-Aamir] <[ummeareesh691@gmail.com]>
2024-07-05 16:38:14 +0500

    16th  Assignment

---
+++ main.ts
@@ -0,0 +1,31 @@
+//    Assignmeny 16
+
+// add more guest for dinner in guestlist
+
+let guestList :string[] = ["Aisha" ,"maheen", "saba","nasra" , "Mariam Aamir"];
+
+
+
+// print message to  inform your guest that place is bigger now many people is invited
+
+console.log("Great news my Dear Friends ,I arange the the bigger table for dinner" )
+
+//add new guest in the beginning of your array using un shift meathod
+
+guestList.unshift("sonia");
+
+//add new guest in the middle and end of your array list and use append to add new guest 
+//splice() add name in middle in list
+
+guestList.splice(Math.floor(guestList.length / 2),0 , "sidra");
+
+//push to add name  last in list
+
+ guestList.push("salma"); 
+
+ //print a new  invitation message
+
+guestList.forEach(guest => {
+     console.log(`Dear ${guest}, you  are cordially invited Dinner `);
+ 
+});
